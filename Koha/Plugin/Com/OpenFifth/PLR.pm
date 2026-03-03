30ce39d86cc5b7327b646acf0d3cf00ad9357e49
Martin Renvoize <martin.renvoize@openfifth.co.uk>
2026-03-03 14:30:26 +0000

    feat: add multi-report support with library_groups scoping
    Replace single authority config with a JSON array of report configs
    (reports_config), each optionally scoped to a library_groups.parent_id.
    Upgrade logic migrates existing single-authority installs automatically.
    
    - PLR.pm v1.2.0: add _get_reports_config, _generate_plr_report,
      _generate_all_reports, _send_single_report, _send_all_reports; update
      install/upgrade/configure/report/cronjob_nightly; keep send_plr_report
      as a backwards-compat wrapper
    - configure.tt: replace authority fieldset with dynamic reports table;
      JS uses DOM methods for add/remove/reindex rows
    - report.tt: loop reports_data for per-report blocks with individual
      download buttons; single Send All Reports button; loop send_results

--- Koha/Plugin/Com/OpenFifth/PLR.pm
+++ Koha/Plugin/Com/OpenFifth/PLR.pm
@@ -9,6 +9,7 @@ use Koha::DateUtils qw(dt_from_string output_pref);
 use MIME::Lite;
 use DateTime;
 use File::Spec;
+use JSON qw(encode_json decode_json);
 
 our $VERSION = '1.1.0';
 our $MINIMUM_VERSION = "22.05.00.000";
@@ -38,12 +39,10 @@ sub new {
 sub install {
     my ( $self, $args ) = @_;
 
-    # Set default configuration values
     $self->store_data({
-        authority_code => '',
-        authority_name => '',
-        email_recipients => 'Joanne.Hawkins@bl.uk,hosting@openfifth.co.uk,plrlibrary@bl.uk',
-        email_from => 'mail@openfifth.co.uk',
+        reports_config    => encode_json([]),
+        email_recipients  => 'Joanne.Hawkins@bl.uk,hosting@openfifth.co.uk,plrlibrary@bl.uk',
+        email_from        => 'mail@openfifth.co.uk',
         auto_send_enabled => 0,
         send_day_of_month => 1,
     });
@@ -53,6 +52,24 @@ sub install {
 
 sub upgrade {
     my ( $self, $args ) = @_;
+
+    my $existing = $self->retrieve_data('reports_config');
+    my $old_code = $self->retrieve_data('authority_code');
+
+    if ( !$existing && $old_code ) {
+        my $old_name = $self->retrieve_data('authority_name') // '';
+        $self->store_data({
+            reports_config => encode_json([{
+                authority_code   => $old_code,
+                authority_name   => $old_name,
+                library_group_id => '',
+            }])
+        });
+    }
+    elsif ( !$existing ) {
+        $self->store_data({ reports_config => encode_json([]) });
+    }
+
     return 1;
 }
 
@@ -69,10 +86,9 @@ sub configure {
         my $template = $self->get_template({ file => 'templates/configure.tt' });
 
         $template->param(
-            authority_code => $self->retrieve_data('authority_code'),
-            authority_name => $self->retrieve_data('authority_name'),
-            email_recipients => $self->retrieve_data('email_recipients'),
-            email_from => $self->retrieve_data('email_from'),
+            reports           => $self->_get_reports_config(),
+            email_recipients  => $self->retrieve_data('email_recipients'),
+            email_from        => $self->retrieve_data('email_from'),
             auto_send_enabled => $self->retrieve_data('auto_send_enabled'),
             send_day_of_month => $self->retrieve_data('send_day_of_month'),
         );
@@ -80,11 +96,28 @@ sub configure {
         $self->output_html( $template->output() );
     }
     else {
+        my $report_count = $cgi->param('report_count') || 0;
+        my @reports;
+
+        for my $i ( 0 .. $report_count - 1 ) {
+            my $code     = $cgi->param("authority_code_$i")   // '';
+            my $name     = $cgi->param("authority_name_$i")   // '';
+            my $group_id = $cgi->param("library_group_id_$i") // '';
+
+            # Validate group_id is numeric or blank
+            $group_id = '' unless $group_id =~ /^\d+$/;
+
+            push @reports, {
+                authority_code   => $code,
+                authority_name   => $name,
+                library_group_id => $group_id,
+            };
+        }
+
         $self->store_data({
-            authority_code => $cgi->param('authority_code'),
-            authority_name => $cgi->param('authority_name'),
-            email_recipients => $cgi->param('email_recipients'),
-            email_from => $cgi->param('email_from'),
+            reports_config    => encode_json(\@reports),
+            email_recipients  => $cgi->param('email_recipients'),
+            email_from        => $cgi->param('email_from'),
             auto_send_enabled => $cgi->param('auto_send_enabled') ? 1 : 0,
             send_day_of_month => $cgi->param('send_day_of_month'),
         });
@@ -99,46 +132,39 @@ sub report {
     my $action = $cgi->param('action') || '';
 
     if ( $action eq 'generate' ) {
-        my $report_data = $self->generate_plr_report();
-
-        if ( $report_data ) {
-            my $template = $self->get_template({ file => 'templates/report.tt' });
-            $template->param(
-                report_generated => 1,
-                report_content => $report_data->{content},
-                report_filename => $report_data->{filename},
-                isbn_count => $report_data->{isbn_count},
-                issue_count => $report_data->{issue_count},
-                start_date => $report_data->{start_date},
-                end_date => $report_data->{end_date},
-            );
-            $self->output_html( $template->output() );
-        }
-        else {
-            my $template = $self->get_template({ file => 'templates/report.tt' });
-            $template->param(
-                error => 'Failed to generate report. Please check configuration.',
-            );
-            $self->output_html( $template->output() );
-        }
-    }
-    elsif ( $action eq 'download' ) {
-        my $report_data = $self->generate_plr_report();
-        if ( $report_data ) {
-            print $cgi->header(
-                -type => 'text/plain',
-                -attachment => $report_data->{filename},
-            );
-            print $report_data->{content};
-        }
-    }
-    elsif ( $action eq 'send' ) {
-        my $result = $self->send_plr_report();
+        my $reports_data = $self->_generate_all_reports();
         my $template = $self->get_template({ file => 'templates/report.tt' });
         $template->param(
-            email_sent => $result->{success},
-            email_error => $result->{error},
+            reports_generated => 1,
+            reports_data      => $reports_data,
         );
+        $self->output_html( $template->output() );
+    }
+    elsif ( $action eq 'download' ) {
+        my $report_index = $cgi->param('report_index') // 0;
+        my $configs      = $self->_get_reports_config();
+        my $config       = $configs->[$report_index];
+
+        if ( $config ) {
+            my $report_data = $self->_generate_plr_report($config);
+            if ( $report_data ) {
+                print $cgi->header(
+                    -type       => 'text/plain',
+                    -attachment => $report_data->{filename},
+                );
+                print $report_data->{content};
+                return;
+            }
+        }
+
+        my $template = $self->get_template({ file => 'templates/report.tt' });
+        $template->param( error => 'Failed to generate report for download.' );
+        $self->output_html( $template->output() );
+    }
+    elsif ( $action eq 'send' ) {
+        my $send_results = $self->_send_all_reports();
+        my $template = $self->get_template({ file => 'templates/report.tt' });
+        $template->param( send_results => $send_results );
         $self->output_html( $template->output() );
     }
     else {
@@ -147,11 +173,20 @@ sub report {
     }
 }
 
-sub generate_plr_report {
-    my ( $self ) = @_;
+# --- Private helpers ---
 
-    my $authority_code = $self->retrieve_data('authority_code');
-    my $authority_name = $self->retrieve_data('authority_name');
+sub _get_reports_config {
+    my ( $self ) = @_;
+    my $json = $self->retrieve_data('reports_config') || encode_json([]);
+    return decode_json($json);
+}
+
+sub _generate_plr_report {
+    my ( $self, $report_config ) = @_;
+
+    my $authority_code   = $report_config->{authority_code}   // '';
+    my $authority_name   = $report_config->{authority_name}   // '';
+    my $library_group_id = $report_config->{library_group_id} // '';
 
     unless ( $authority_code && $authority_name ) {
         warn "PLR Plugin: Authority code and name must be configured";
@@ -161,82 +196,118 @@ sub generate_plr_report {
     my $dbh = C4::Context->dbh;
 
     # Calculate date range (previous month)
-    my $now = DateTime->now();
+    my $now        = DateTime->now();
     my $last_month = $now->clone->subtract(months => 1);
 
     my $start_date = $last_month->clone->set_day(1);
-    my $end_date = $last_month->clone->add(months => 1)->set_day(1);
+    my $end_date   = $last_month->clone->add(months => 1)->set_day(1);
 
-    # Format dates for output
     my $start_date_formatted = $start_date->strftime('%d%m%Y');
-    my $end_date_prev = $end_date->clone->subtract(days => 1);
-    my $end_date_formatted = $end_date_prev->strftime('%d%m%Y');
+    my $end_date_prev        = $end_date->clone->subtract(days => 1);
+    my $end_date_formatted   = $end_date_prev->strftime('%d%m%Y');
 
-    # SQL query to get PLR data
-    my $sql = q{
-        SELECT
-            COUNT(s.itemnumber) AS COUNT,
-            bi.isbn AS ISBN,
-            UPPER(SUBSTRING(MAX(b.author),1,4)) AS AUTHOR,
-            s.itemtype AS ITEMTYPE,
-            MAX(s.itemnumber) AS ITEMNUMBER,
-            MAX(i.biblionumber) AS BIBLIONUMBER
-        FROM statistics s
-        LEFT JOIN items i ON (s.itemnumber = i.itemnumber)
-        LEFT JOIN biblioitems bi ON (i.biblionumber = bi.biblionumber)
-        LEFT JOIN biblio b ON (i.biblionumber = b.biblionumber)
-        WHERE s.datetime >= ?
-            AND s.datetime < ?
-            AND s.type IN ('issue', 'renew')
-            AND bi.isbn IS NOT NULL
-        GROUP BY bi.isbn, s.itemtype
-        ORDER BY bi.isbn, s.itemtype
-    };
+    # Build main SQL – optionally scoped to a library group
+    my ( $sql, @bind );
+    if ( $library_group_id ) {
+        $sql = q{
+            SELECT
+                COUNT(s.itemnumber) AS COUNT,
+                bi.isbn AS ISBN,
+                UPPER(SUBSTRING(MAX(b.author),1,4)) AS AUTHOR,
+                s.itemtype AS ITEMTYPE,
+                MAX(s.itemnumber) AS ITEMNUMBER,
+                MAX(i.biblionumber) AS BIBLIONUMBER
+            FROM statistics s
+            LEFT JOIN items i ON (s.itemnumber = i.itemnumber)
+            LEFT JOIN library_groups lg ON (s.branch = lg.branchcode)
+            LEFT JOIN biblioitems bi ON (i.biblionumber = bi.biblionumber)
+            LEFT JOIN biblio b ON (i.biblionumber = b.biblionumber)
+            WHERE s.datetime >= ?
+                AND s.datetime < ?
+                AND s.type IN ('issue', 'renew')
+                AND bi.isbn IS NOT NULL
+                AND lg.parent_id = ?
+            GROUP BY bi.isbn, s.itemtype
+            ORDER BY bi.isbn, s.itemtype
+        };
+        @bind = ( $start_date->ymd, $end_date->ymd, $library_group_id );
+    }
+    else {
+        $sql = q{
+            SELECT
+                COUNT(s.itemnumber) AS COUNT,
+                bi.isbn AS ISBN,
+                UPPER(SUBSTRING(MAX(b.author),1,4)) AS AUTHOR,
+                s.itemtype AS ITEMTYPE,
+                MAX(s.itemnumber) AS ITEMNUMBER,
+                MAX(i.biblionumber) AS BIBLIONUMBER
+            FROM statistics s
+            LEFT JOIN items i ON (s.itemnumber = i.itemnumber)
+            LEFT JOIN biblioitems bi ON (i.biblionumber = bi.biblionumber)
+            LEFT JOIN biblio b ON (i.biblionumber = b.biblionumber)
+            WHERE s.datetime >= ?
+                AND s.datetime < ?
+                AND s.type IN ('issue', 'renew')
+                AND bi.isbn IS NOT NULL
+            GROUP BY bi.isbn, s.itemtype
+            ORDER BY bi.isbn, s.itemtype
+        };
+        @bind = ( $start_date->ymd, $end_date->ymd );
+    }
 
     my $sth = $dbh->prepare($sql);
-    $sth->execute($start_date->ymd, $end_date->ymd);
+    $sth->execute(@bind);
 
     my @report_lines;
-    my $isbn_count = 0;
+    my $isbn_count  = 0;
     my $issue_count = 0;
 
     # Header line
     push @report_lines, "$authority_code|$start_date_formatted|$end_date_formatted";
 
-    # Process each ISBN
     while ( my $row = $sth->fetchrow_hashref ) {
-        my $isbn = $row->{ISBN};
-        my $contributor = $row->{AUTHOR};
+        my $isbn         = $row->{ISBN};
+        my $contributor  = $row->{AUTHOR};
         my $biblionumber = $row->{BIBLIONUMBER};
-        my $itemtype = $row->{ITEMTYPE};
-        my $no_issues = $row->{COUNT};
+        my $itemtype     = $row->{ITEMTYPE};
+        my $no_issues    = $row->{COUNT};
 
-        # Extract real ISBN (13 or 10 digit)
         my $real_isbn;
         if ( $isbn =~ /^978/ ) {
-            # 13 digit ISBN
             ($real_isbn) = $isbn =~ /^(\d{13})/;
         }
         else {
-            # 10 digit ISBN
             ($real_isbn) = $isbn =~ /^(\d{10})/;
         }
 
         next unless $real_isbn;
 
-        # Get copy count for this biblionumber
-        my $copy_count_sql = q{
-            SELECT COUNT(itemnumber)
-            FROM items
-            WHERE biblionumber = ?
-        };
+        # Copy count – also scoped to library group if set
+        my ( $copy_count_sql, @copy_bind );
+        if ( $library_group_id ) {
+            $copy_count_sql = q{
+                SELECT COUNT(i.itemnumber)
+                FROM items i
+                LEFT JOIN library_groups lg ON (i.homebranch = lg.branchcode)
+                WHERE i.biblionumber = ?
+                  AND lg.parent_id = ?
+            };
+            @copy_bind = ( $biblionumber, $library_group_id );
+        }
+        else {
+            $copy_count_sql = q{
+                SELECT COUNT(itemnumber)
+                FROM items
+                WHERE biblionumber = ?
+            };
+            @copy_bind = ($biblionumber);
+        }
+
         my $copy_sth = $dbh->prepare($copy_count_sql);
-        $copy_sth->execute($biblionumber);
+        $copy_sth->execute(@copy_bind);
         my ($copy_count) = $copy_sth->fetchrow_array;
 
-        # Build data line
         push @report_lines, "$real_isbn|$no_issues|$copy_count|$contributor|$itemtype";
-
         $isbn_count++;
         $issue_count += $no_issues;
     }
@@ -244,40 +315,60 @@ sub generate_plr_report {
     # Footer line
     push @report_lines, "$isbn_count|$issue_count";
 
-    my $content = join("\n", @report_lines) . "\n";
-    my $today = DateTime->now()->strftime('%d%m%Y');
-    my $filename = "plrdata_${today}.txt";
+    my $content  = join("\n", @report_lines) . "\n";
+    my $today    = DateTime->now()->strftime('%d%m%Y');
+    my $filename = "plrdata_${today}_${authority_code}.txt";
 
     return {
-        content => $content,
-        filename => $filename,
-        isbn_count => $isbn_count,
-        issue_count => $issue_count,
-        start_date => $start_date_formatted,
-        end_date => $end_date_formatted,
+        content        => $content,
+        filename       => $filename,
+        isbn_count     => $isbn_count,
+        issue_count    => $issue_count,
+        start_date     => $start_date_formatted,
+        end_date       => $end_date_formatted,
+        authority_code => $authority_code,
+        authority_name => $authority_name,
     };
 }
 
-sub send_plr_report {
+sub _generate_all_reports {
     my ( $self ) = @_;
 
-    my $authority_code = $self->retrieve_data('authority_code');
-    my $authority_name = $self->retrieve_data('authority_name');
+    my $configs = $self->_get_reports_config();
+    my @results;
+    my $index = 0;
+
+    for my $config ( @$configs ) {
+        my $data = $self->_generate_plr_report($config);
+        if ( $data ) {
+            $data->{report_index} = $index;
+            push @results, $data;
+        }
+        $index++;
+    }
+
+    return \@results;
+}
+
+sub _send_single_report {
+    my ( $self, $report_data ) = @_;
+
     my $email_recipients = $self->retrieve_data('email_recipients');
-    my $email_from = $self->retrieve_data('email_from');
+    my $email_from       = $self->retrieve_data('email_from');
 
-    unless ( $authority_code && $authority_name && $email_recipients && $email_from ) {
-        return { success => 0, error => 'Missing required configuration' };
+    unless ( $email_recipients && $email_from ) {
+        return {
+            success        => 0,
+            error          => 'Missing email configuration',
+            authority_code => $report_data->{authority_code},
+            authority_name => $report_data->{authority_name},
+        };
     }
 
-    my $report_data = $self->generate_plr_report();
-    unless ( $report_data ) {
-        return { success => 0, error => 'Failed to generate report' };
-    }
-
-    # Create email
-    my $subject = "PLR Return for $authority_code - $authority_name";
-    my $body = "Dear PLR\n\nPlease find attached PLR return for $authority_code - $authority_name for the last period\n\n";
+    my $authority_code = $report_data->{authority_code};
+    my $authority_name = $report_data->{authority_name};
+    my $subject        = "PLR Return for $authority_code - $authority_name";
+    my $body           = "Dear PLR\n\nPlease find attached PLR return for $authority_code - $authority_name for the last period\n\n";
 
     my $msg = MIME::Lite->new(
         From    => $email_from,
@@ -286,13 +377,11 @@ sub send_plr_report {
         Type    => 'multipart/mixed',
     );
 
-    # Add body
     $msg->attach(
         Type => 'TEXT',
         Data => $body,
     );
 
-    # Add attachment
     $msg->attach(
         Type        => 'text/plain',
         Data        => $report_data->{content},
@@ -300,35 +389,74 @@ sub send_plr_report {
         Disposition => 'attachment',
     );
 
-    # Send email
-    eval {
-        $msg->send;
-    };
+    eval { $msg->send };
 
     if ( $@ ) {
-        return { success => 0, error => "Email send failed: $@" };
+        return {
+            success        => 0,
+            error          => "Email send failed: $@",
+            authority_code => $authority_code,
+            authority_name => $authority_name,
+        };
     }
 
-    return { success => 1 };
+    return {
+        success        => 1,
+        authority_code => $authority_code,
+        authority_name => $authority_name,
+    };
+}
+
+sub _send_all_reports {
+    my ( $self ) = @_;
+
+    my $configs = $self->_get_reports_config();
+    my @results;
+
+    for my $config ( @$configs ) {
+        my $report_data = $self->_generate_plr_report($config);
+        if ( !$report_data ) {
+            push @results, {
+                success        => 0,
+                error          => 'Failed to generate report',
+                authority_code => $config->{authority_code} // '',
+                authority_name => $config->{authority_name} // '',
+            };
+            next;
+        }
+        push @results, $self->_send_single_report($report_data);
+    }
+
+    return \@results;
+}
+
+# Public backwards-compat wrapper
+sub send_plr_report {
+    my ( $self ) = @_;
+    my $results = $self->_send_all_reports();
+    return $results->[0] // { success => 0, error => 'No reports configured' };
 }
 
 sub cronjob_nightly {
     my ( $self ) = @_;
 
     my $auto_send_enabled = $self->retrieve_data('auto_send_enabled');
-    my $send_day = $self->retrieve_data('send_day_of_month');
+    my $send_day          = $self->retrieve_data('send_day_of_month');
 
     return unless $auto_send_enabled;
 
     my $today = DateTime->now()->day;
 
     if ( $today == $send_day ) {
-        my $result = $self->send_plr_report();
-        if ( $result->{success} ) {
-            warn "PLR Plugin: Report sent successfully on day $send_day";
-        }
-        else {
-            warn "PLR Plugin: Failed to send report: " . $result->{error};
+        my $results = $self->_send_all_reports();
+        for my $result ( @$results ) {
+            if ( $result->{success} ) {
+                warn "PLR Plugin: Report for $result->{authority_code} sent successfully on day $send_day";
+            }
+            else {
+                warn "PLR Plugin: Failed to send report for $result->{authority_code}: "
+                    . ( $result->{error} // 'unknown error' );
+            }
         }
     }
 }
